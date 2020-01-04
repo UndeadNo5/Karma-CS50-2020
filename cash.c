4429b8d6f6bc736ba3dedf2379fd231e4248e5b2
LadyKarmA <59354240+LadyKarmA@users.noreply.github.com>
2020-01-04 13:59:02 -0700

    Add files via upload

---
+++ cash.c
@@ -0,0 +1,31 @@
+//Greed Algorithm: Find out the least amount of change possible 
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    float dollar = -1;
+// Get user input of a positive float    
+    while (dollar < 0.009)
+    {
+        dollar = get_float("Change: ");
+    }
+// Change $ into a interger    
+    int change = round(dollar * 100);
+// Define and assign value to coins while subtracting the value from the original change
+    int quarter = change / 25;
+    change = change - quarter * 25;
+    int dimes = change / 10;
+    change = change - dimes * 10;
+    int nickels = change / 5;
+    change = change - nickels * 5;
+    int pennies = change / 1;
+    change = change - pennies * 1;
+// Add all the coins up to find the least number of coins possible    
+    int coins = quarter + dimes + nickels + pennies;
+// Command if you want to display all the denominations of individual coins
+//     printf("You will need %i quarters, %i dimes , %i nickels and %i pennies \n", quarter, dimes, nickels, pennies);      
+    printf("%i \n", coins);
+}    
+
